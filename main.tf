5ac586272651f194e2be81af85cd6107d13ca9ca
Tharun-Ande <a.tarun77@gmail.com>
2025-12-01 14:46:39 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 #create vpc
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = var.vpc_tenancy
   tags = {
-    Name = "tharun-vpc"
+    Name = var.vpc_tag
   }
 }
 
 #create public subnet
 resource "aws_subnet" "tharun_public" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_cidr_block
   tags = {
-    Name = "public subnet"
+    Name = var.subnet_tag
   }
 }
 
 #create private subnet
 resource "aws_subnet" "tharun_private" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.subnet_cidr_block_pvt
   tags = {
-    Name = "private subnet"
+    Name = var.subnet_tag_pvt
   }
 }
 
@@ -29,7 +29,7 @@ resource "aws_subnet" "tharun_private" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
   tags = {
-    Name = "igw"
+    Name = var.igw_tag
   }
 }
 
@@ -37,11 +37,11 @@ resource "aws_internet_gateway" "gw" {
  resource"aws_route_table" "tharun_public" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.pb_rt_cidr_blk
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
-    Name = "tharun_public"
+    Name = var.pb_rt_tag
   }
 } 
 
@@ -49,11 +49,11 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "tharun_private" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.pvt_rt_cidr_blk
     gateway_id = aws_nat_gateway.tharun.id
   }
   tags = {
-    Name = "tharun_private"
+    Name = var.pvt_rt_tag
   }
 }
  
@@ -70,7 +70,7 @@ resource "aws_route_table_association" "private" {
 #Attach Elastic IP
 resource "aws_eip" "tharun" {
   tags = {
-    Name = "t-eip"
+    Name = var.eip_tag
   }
 }
 
@@ -79,23 +79,23 @@ resource "aws_nat_gateway" "tharun" {
   allocation_id = aws_eip.tharun.id
   subnet_id     = aws_subnet.tharun_private.id
   tags = {
-    Name = "gw NAT"
+    Name = var.n_gw
   }
 }
 
 #create a Key pair
 resource "aws_key_pair" "t_key" {
-  key_name   = "tharun-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAADzBwzt7Qyaq2jPfg/HW1lOdnCCHp4jmnICK4EK5FDILrkxrr2gKd/703xs3xqQzFJq0qtIV+PGnosen4MDE7vSlU92NLDGh7UqJkyqZ5c2pgIoH1KSiWBMLkgSZDIup2vmQzXwy9nU8TH5PfN3yCTnwp2FfDD3WfE2N++tW3SoeiFYJ0NsDGMES3SMT4e3dKBfGe6R3eJDqtideSFTil58Q+ijW5UccNpWDRvLU6iVbOPoYwl7jvg+YVRAYasAA4qIE0Wfno5/br8z2IXk32DpOfmKCex4xuJqus9DqAmgPkxJqnuKswhZXs0Fm4l/fYQqiLORM7OOn7WqlnTCgZGSfDFq4MsR/1f4eeOI35L10QceD3h1bMGS7fITSlMgb6SvR7MJ0AZOLV+envVSH38UQcSEPNVktFQ6zHs793ehWyGfU05jjbRB3VqceSd0J5c90MHOxSs9CgOgIwqbLMaG8S/M9LbKdpONHeZdUnFCQkT8/maz4BJGx55WfjGayA93l6DYXpODQp8Jz3aMfjafLim08g7H25v9eAYbXHoQaU/FtCKMzHbSPSJWvEaqP+1NQN/gVySfKcFSCRpyh6bkOkALB6y4iuEwIxpDNFULiQIssm+x6TN3k6/wBTs4CH5iLnxVFGSdzqjEAZpIZn/Q1DebnB7gQoL8cZer9M9p andec@CHARAN"
+  key_name   = var.key
+  public_key = var.p_key
 }
 
 #create Security group with inbound and outbound rules
 resource "aws_security_group" "t_sg" {
-  name        = "t_sg"
+  name        = var.sg
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.main.id
   tags = {
-    Name = "t_sg"
+    Name = var.sg_tag
   }
   ingress {
     from_port        = 22
@@ -122,26 +122,25 @@ resource "aws_security_group" "t_sg" {
 
 #create a public instance
 resource "aws_instance" "tharun" {
-  ami           = "ami-0fa91bc90632c73c9"
-  instance_type = "t3.micro"
+  ami           = var.ami
+  instance_type = var.i
   vpc_security_group_ids = [aws_security_group.t_sg.id]
   subnet_id = aws_subnet.tharun_public.id
-  key_name = "tharun-key"
-  associate_public_ip_address = true
+  key_name = var.key_in
+  associate_public_ip_address = var.x
   tags = {
-    Name = "tharun"
-  }
+    Name = var.n_pub_tag  }
 }
 
 #create a private instance
 resource "aws_instance" "tharun_pvt" {
-  ami           = "ami-0fa91bc90632c73c9"
-  instance_type = "t3.micro"
+  ami           = var.ami_pvt
+  instance_type = var.i
   vpc_security_group_ids = [aws_security_group.t_sg.id]
   subnet_id = aws_subnet.tharun_private.id
-  key_name = "tharun-key"
-  associate_public_ip_address = false
+  key_name = var.key_in
+  associate_public_ip_address = var.y
   tags = {
-    Name = "tharun_pvt"
+    Name = var.n_pvt_tag 
   }
 }
